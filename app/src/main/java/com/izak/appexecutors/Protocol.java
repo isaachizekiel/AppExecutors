4fa61671709ae4502659a47e350488585e109c32
ezak <izak.java@gmail.com>
2022-03-14 11:22:25 +0300

    decode plan

--- app/src/main/java/com/izak/appexecutors/Protocol.java
+++ app/src/main/java/com/izak/appexecutors/Protocol.java
@@ -7,8 +7,12 @@ public class Protocol {
     Protocol(ProtocolListener listener) {this.listener = listener; }
 
     void decode(String protocol) {
-
         // decode json
+        // ?(protocol only pk) -> { show send fund dialog..
+        // ?(send)-> { encrypt_value, sign, ~push_to_db, ~show_qr }
+        // ?(cancel)-> }
+        //
+        // ?(protocol only cip and sig) -> { verify, decrypt, push to db}
 
 
         listener.onProtocol(protocol);
